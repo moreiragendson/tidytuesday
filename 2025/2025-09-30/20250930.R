f9657bfd951567eea8da3ca0b7370bd72b51d995
Gendson Moreira <gendsonmoreira@outlook.com>
2025-10-04 15:23:42 -0300

    fix 20250930

--- 2025/2025-09-30/20250930.R
+++ 2025/2025-09-30/20250930.R
@@ -11,17 +11,25 @@ library(showtext)
 tuesdata <- tidytuesdayR::tt_load("2025-09-30")
 cranes <- tuesdata$cranes
 
+# Option 2: Read directly from GitHub
+
+# cranes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-09-30/cranes.csv')
+
+
 # Load fonts -------------------------------------------------------------
 
 font_add_google("Lato", "lato")
 
 # Define colors and fonts ------------------------------------------------
 
-bg_color <- "#001219"
-txt_color <- "#ffffff"
-color_one <- "#fdbf11"
-color_two <- "#1696d2"
+bg_color <- "#ffffff"
+txt_color <- "#001219"
+color_one <- "#1696d2"
+color_two <- "#001219"
+color_three <- "#d2d2d2"
 txt_font <- "lato"
+title_font_size <- 16
+st_font_size <- 12
 
 # Data wrangling ---------------------------------------------------------
 
@@ -32,18 +40,28 @@ grouped_data <- cranes |>
     year = year(date),
     day_of_year = yday(date),
     month = month(date, label = TRUE, abbr = FALSE),
-    year_month = floor_date(date, "month")
+    year_month = floor_date(date, "month"),
+    season = case_when(
+      month(date) %in% 3:5 ~ "Spring",
+      month(date) %in% 9:11 ~ "Fall",
+      TRUE ~ "Other"),
+    season = factor(season, levels = c("Spring", "Fall", "Other"))
 ) |> 
-  group_by(year) |> 
+  group_by(year, season) |> 
   summarise(count = sum(observations, na.rm = TRUE))
 
 
-# Scales and texts -------------------------------------------------------
+
+# Set scales -------------------------------------------------------------
+
 
 min_year <- grouped_data |> pull(year) |> min()
 max_year <- grouped_data |> pull(year) |> max()
-
 max_count <- grouped_data |> pull(count) |> max()
+
+
+# Set texts --------------------------------------------------------------
+
 
 title_txt <- "Crane Observations at Lake Hornborgasjön, Sweden (1994–2024)"
 st_txt <- "For more than 30 years, cranes stopping at the Lake Hornborgasjön ('Lake Hornborga') in Västergötland, Sweden have been \ncounted from the Hornborgasjön field station in the spring and the fall as they pass by during their yearly migration.\nIn 2019, more than 500 thousands cranes stopped at the Lake Hornborgasjön."
@@ -52,42 +70,29 @@ caption_txt <- "\nData: Transtatistik ('crane statistics'), Naturum, Hornborgasj
 
 # Chart ------------------------------------------------------------------
 
-plt_data <- grouped_data |>
-  mutate(
-    highest_value = case_when(
-      count == max_count ~ as.double(count),
-      TRUE ~ as.double(NA)
-    ),
-    highest_value_in = case_when(
-      count == max_count ~ as.double(year),
-      TRUE ~ as.double(NA)
-    ),
-    cat = ""
-  )
-
-
-ref_year <- plt_data |> pull(highest_value_in) |> max(na.rm = TRUE)
-ref_value <- plt_data |> pull(highest_value) |> max(na.rm = TRUE)
-
-final_plt <- plt_data |>
+final_plt <- grouped_data |>
   ggplot() +
-  geom_area(aes(x = year, y = count), fill = color_two) +
+  geom_area(aes(x = year, y = count, fill = season)) +
   scale_x_continuous(breaks = seq(min_year, max_year, 5)) +
+  scale_fill_manual(values = c("Spring" = color_one, "Fall" = color_two, "Other" = color_three))+
   scale_y_continuous(limits = c(0,6e5))+
-  theme_minimal(base_family = txt_color) +
+  theme_minimal(base_family = txt_font) +
   labs(
     title = title_txt,
     subtitle = st_txt,
     caption = caption_txt
   ) +
   theme(
-    plot.title = element_text(face = "bold", color = txt_color, size = 16),
-    plot.subtitle = element_text(face = "plain", color = txt_color, size = 12),
+    plot.title = element_text(face = "bold", color = txt_color, size = title_font_size),
+    plot.subtitle = element_text(face = "italic", color = txt_color, size = st_font_size),
     plot.caption = element_text(color = txt_color),
     plot.caption.position = "plot",
-    legend.position = "none",
+    legend.position = "top",
+    legend.text = element_text(color = txt_color),
+    legend.title = element_blank(),
     plot.background = element_rect(fill = bg_color),
     axis.text.x = element_text(
+      family = txt_font,
       face = "bold",
       color = txt_color
     ),
